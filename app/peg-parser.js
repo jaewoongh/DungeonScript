07ea3bb28f61d548984ba7d9a7fc50a8708df44c
jaewoongh <jaewoong.here@gmail.com>
2014-05-04 12:35:06 -0400

    i'm confused now about the grammar

--- app/peg-parser.js
+++ app/peg-parser.js
@@ -3,11 +3,12 @@ var PEG             = require('pegjs');
 
 // Grammar
 var commonGrammar   = " \
-query       = conditionQ / tagQ                                                                                     \
+query       = conditionQ / tagQ / notTagQ                                                                           \
 conditionQ  = t:text space s:compareSign space v:value space                                                        \
                                                     { return t+':function(a){return a'+s+v+';}' }                   \
 tagQ        = t:text space                          { return t+':true' }                                            \
-value       = a:(literal / block) b:(mathSign (literal / block))*                                                   \
+notTagQ     = '!' t:text space                      { return t+':undefined'}                                        \
+value       = a:(literal / block) space b:(mathSign space (literal / block))*                                       \
                                                     { return a+b.join('').replace(/,/g, '') }                       \
 block       = '(' v:value ')'                       { return '('+v+')' }                                            \
 literal     = color / string / const / bool / number                                                                \
@@ -22,12 +23,12 @@ string      = doubleQuot / singleQuot                                           
 doubleQuot  = '\"' s:(text / dqText)+ '\"' space    { return \"'\"+s+\"'\" }                                        \
 singleQuot  = \"'\" s:(text / sqText)+ \"'\" space  { return \"'\"+s+\"'\" }                                        \
 text        = t:legalChar+ space                    { return t.join('') }                                           \
-legalChar   = [a-zA-Z0-9_\]                                                                                        \
+legalChar   = [a-zA-Z0-9_\]                                                                                         \
 dqText      = t:textCharDbl+ space                  { return t.join('') }                                           \
 sqText      = t:textCharSgl+ space                  { return t.join('') }                                           \
 textCharDbl = [^\\r\\n\"]                                                                                           \
 textCharSgl = [^\\r\\n']                                                                                            \
-const       = c:'remove'                            { return \"'__\"+c+\"__'\" }                                    \
+const       = c:'remove'                            { return 'undefined' }                                          \
 bool        = 'true' / 'false'                                                                                      \
 number      = real / integer                                                                                        \
 integer     = s:'-'? d:digit+ space                 { return parseInt(s?s:'' +d.join(''), 10) }                     \
@@ -39,8 +40,8 @@ SPACE       = [ ,\\r\\n]+ / !.                                                  
 
 var parser4meta     = PEG.buildParser(" \
 start       = def:(charmAssign / charmTag)+ space   { return 'queryThings(this,{meta:true},function(there){'+def.join(';')+';});' }     \
-charmTag    = t:text space                          { return 'there.'+t+'=true' }                                    \
-charmAssign = t:text space ':' space v:value space  { return 'there.'+t+'='+v }                                      \
+charmTag    = t:text space                          { return 'there.'+t+'=true' }                                                       \
+charmAssign = t:text space ':' space v:value space  { return 'there.'+t+'='+v }                                                         \
 "+commonGrammar);
 
 var parser4things   = PEG.buildParser(" \
@@ -54,19 +55,21 @@ atRefer     = '@' q:query                           { return 'function(){return 
 "+commonGrammar);
 
 var parser4gameloop = PEG.buildParser(" \
-start       = p:queryAndDo*                         { return p.join('') }                                               \
-queryAndDo  = q:query+ '{' space d:(putCharm / modify)+ '}' space                                                       \
-                                                    { return 'queryThings({'+q+'},function(){'+d.join(';')+'});' }      \
-modify      = charmModify / tagModify                                                                                   \
-putCharm    = t:text '<-' space d:thingModify       { return 'todo.push(queryThings.bind(this,this,{'+t+':true},function(there){'+d+'}));' } \
-tagModify   = r:refer space                         { return r+'=true' }                                                \
-charmModify = r:refer space ':' space v:(value/refer) space                                                             \
-                                                    { return r+'='+v }                                                  \
-thingModify = '{' space def:(charmModify / tagModify)+ '}' space                                                        \
-                                                    { return '{'+def.join(';')+'}' }                                    \
-refer       = dotRefer / localRefer                                                                                     \
-dotRefer    = '.' t:text                            { return 'this.'+t }                                                \
-localRefer  = t:text                                { return 'there.'+t }                                               \
+start       = p:queryAndDo*                         { return p.join('') }                                                                       \
+queryAndDo  = q:query+ '{' space d:(putCharm / modify)+ '}' space                                                                               \
+                                                    { return 'queryThings({'+q+'},function(){'+d.join(';')+'});' }                              \
+modify      = charmModify / tagModify                                                                                                           \
+tagModify   = r:dotRefer space                      { return r+'=true' }                                                                        \
+charmModify = r:dotRefer space ':' space v:(value / dotRefer) space                                                                             \
+                                                    { return r+'='+v }                                                                          \
+putCharm    = q:query+ '<-' space d:thingPass       { return 'todo.push(queryThings.bind(this,this,{'+q+'},function(there){'+d+'}));' }    \
+thingPass   = '{' space def:(charmPass / tagPass)+ '}' space                                                                                    \
+                                                    { return '{'+def.join(';')+'}' }                                                            \
+tagPass     = r:(dotRefer / localRefer) space       { return r+'=true' }                                                                        \
+charmPass   = r:(dotRefer / localRefer) space ':' space v:(value / dotRefer / localRefer) space                                                 \
+                                                    { return r+'='+v }                                                                          \
+dotRefer    = '.' t:text                            { return 'this.'+t }                                                                        \
+localRefer  = t:text                                { return 'there.'+t }                                                                       \
 "+commonGrammar);
 
 // Export functions
